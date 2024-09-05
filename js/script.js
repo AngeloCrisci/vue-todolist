ae5020806a5bc93e85810a95991e213580835641
Angelo.Crisci <angelocrisci1998@gmail.com>
2024-09-05 16:46:07 +0200

    Added items as a item in Page

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
 console.log('JS OK')
+
+const { createApp } = Vue;
+
+
+createApp({
+    data(){
+        return {
+            NewTodoItem: {item: '', done: ''},
+            toDoList: [
+                {item: 'Fare la spesa', done: true},
+                {item: 'Controllare il contatore', done: false},
+                {item: 'Pagare la bolleta', done: true},
+                {item: 'Fare i compiti a casa', done: false},
+                {item: 'Giocare alla play', done: true},
+                {item: 'Chiamare nonna', done: false},
+            ]
+        }
+     },
+
+     methods: {
+        AddDone
+     }
+
+
+  }).mount('#app')
